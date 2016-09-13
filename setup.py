01d718cb4cd1554cc03fe828372358c4ed498581
oliverxchen <oliverxchen@gmail.com>
2016-09-13 23:05:48 +0800

    futzing around to get packaging approximately working on testpypi

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
 from setuptools import setup
 import os
+import re
 
 
-version_file = os.path.join(
-    os.path.dirname(os.path.abspath(__file__)), 'VERSION')
-version = open(version_file, 'rt').read().strip()
+try:
+    version_file = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), 'VERSION')
+    version = open(version_file, 'rt').read().strip()
+except FileNotFoundError:
+    package_info_file = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), 'PKG-INFO')
+    info = open(package_info_file, 'rt').read()
+    version = re.search('\nVersion: (.*)\n', info).group(1)
 
 install_requires = [
     'backports.functools_lru_cache==1.2.1',
@@ -17,7 +24,7 @@ setup(
     name='test-driven-data-cleaning',
     install_requires=install_requires,
     version=version,
-    package_data={'': ['VERSION']},
+    packages=['tddc'],
     url='https://github.com/DataKind-SG/test-driven-data-cleaning',
     author='Oliver Chen',
     author_email='oliverxchen@gmail.com'
