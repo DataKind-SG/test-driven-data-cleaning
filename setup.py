bbf5225a2c564505486e6e0d48f671fc7cf013e9
oliverxchen <oliverxchen@gmail.com>
2016-09-15 00:34:37 +0800

    update package metadata in setup.py

--- setup.py
+++ setup.py
@@ -20,6 +20,15 @@ install_requires = [
     'PyYAML==3.12',
 ]
 
+long_description = 'This package provides a framework for collaborative,
+test-driven data cleaning. The framework enables a reproducible method for data
+cleaning that can be easily validated. \n\nFor a given tabular data set, a
+Trello board is populated with cards for each column so that team members can
+tag themselves to a column and ensure that work does not overlap. The cards
+include summary statistics of the columns that can be useful for writing
+methods to clean the column. Method stubs and test stubs are also scaffolded
+out for team members to fill out.'
+
 setup(
     name='tddc',
     install_requires=install_requires,
@@ -28,5 +37,20 @@ setup(
     scripts=['bin/tddc'],
     url='https://github.com/DataKind-SG/test-driven-data-cleaning',
     author='Oliver Chen',
-    author_email='oliverxchen@gmail.com'
+    author_email='oliverxchen@gmail.com',
+    description='Scaffold out methods and tests for collaborative data
+    cleaning.',
+    long_description=long_description,
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: DataJam organizers',
+        'Topic :: Data Science :: Data Cleaning Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5'
+    ],
+    keywords='data cleaning collaborative'
 )
