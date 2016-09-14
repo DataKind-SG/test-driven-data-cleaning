bae2adb98cc1786811d7df38cc061abe2b371028
oliverxchen <oliverxchen@gmail.com>
2016-09-15 00:37:10 +0800

    fix setup.py

--- setup.py
+++ setup.py
@@ -20,14 +20,20 @@ install_requires = [
     'PyYAML==3.12',
 ]
 
-long_description = 'This package provides a framework for collaborative,
-test-driven data cleaning. The framework enables a reproducible method for data
-cleaning that can be easily validated. \n\nFor a given tabular data set, a
-Trello board is populated with cards for each column so that team members can
-tag themselves to a column and ensure that work does not overlap. The cards
-include summary statistics of the columns that can be useful for writing
-methods to clean the column. Method stubs and test stubs are also scaffolded
-out for team members to fill out.'
+short_description = 'Scaffold out methods and tests for collaborative data ' \
+                    'cleaning.'
+
+long_description = 'This package provides a framework for collaborative, ' \
+                   'test-driven data cleaning. The framework enables a ' \
+                   'reproducible method for data cleaning that can be easily ' \
+                   'validated. \n\nFor a given tabular data set, a Trello ' \
+                   'board is populated with cards for each column so that ' \
+                   'team members can tag themselves to a column and ensure ' \
+                   'that work does not overlap. The cards include summary ' \
+                   'statistics of the columns that can be useful for writing ' \
+                   'methods to clean the column. Method stubs and test ' \
+                   'stubs are also scaffolded out for team members to fill ' \
+                   'out.'
 
 setup(
     name='tddc',
@@ -38,14 +44,11 @@ setup(
     url='https://github.com/DataKind-SG/test-driven-data-cleaning',
     author='Oliver Chen',
     author_email='oliverxchen@gmail.com',
-    description='Scaffold out methods and tests for collaborative data
-    cleaning.',
+    description=short_description,
     long_description=long_description,
     license='MIT',
     classifiers=[
         'Development Status :: 3 - Alpha',
-        'Intended Audience :: DataJam organizers',
-        'Topic :: Data Science :: Data Cleaning Tools',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.3',
