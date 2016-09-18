50c95d4e1f0057b3e7336125283bf3f2e3eb6341
oliverxchen <oliverxchen@gmail.com>
2016-09-18 19:53:03 +0800

    normalize test file name in fixtures

--- tddc/tests/test_build.py
+++ tddc/tests/test_build.py
@@ -19,17 +19,17 @@ def is_same_file(file_a, file_b):
     return diff_return == 0
 
 
-def test_write_cleaning_script(build_class, fixtures_dir, input_filename):
+def test_write_cleaning_script(build_class, fixtures_dir):
     cleaning_file = build_class.write_cleaning_script()
-    fixture_clean = os.path.join(
-        fixtures_dir, 'clean_' + os.path.splitext(input_filename)[0] + '.txt'
-    )
+    cleaning_base = os.path.splitext(os.path.basename(cleaning_file))[0]
+    fixture_clean = os.path.join(fixtures_dir, cleaning_base + '.txt')
     assert is_same_file(cleaning_file, fixture_clean)
 
 
-def test_write_test_cleaning_script(build_class, fixtures_dir, input_filename):
+def test_write_test_cleaning_script(build_class, fixtures_dir):
     test_cleaning_file = build_class.write_test_cleaning_script()
+    test_cleaning_base = os.path.splitext(
+        os.path.basename(test_cleaning_file))[0]
     fixture_test_clean = os.path.join(
-        fixtures_dir, 't_clean_' + os.path.splitext(input_filename)[0] + '.txt'
-    )
+        fixtures_dir, test_cleaning_base + '.txt')
     assert is_same_file(test_cleaning_file, fixture_test_clean)
