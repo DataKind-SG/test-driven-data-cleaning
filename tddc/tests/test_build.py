8a4f078b6d16a1ce055381961fa388c21ed42ce3
oliverxchen <oliverxchen@gmail.com>
2016-09-18 20:23:57 +0800

    simplify test fixture file names...

--- tddc/tests/test_build.py
+++ tddc/tests/test_build.py
@@ -22,7 +22,7 @@ def is_same_file(file_a, file_b):
 def test_write_cleaning_script(build_class, fixtures_dir):
     cleaning_file = build_class.write_cleaning_script()
     cleaning_base = os.path.splitext(os.path.basename(cleaning_file))[0]
-    fixture_clean = os.path.join(fixtures_dir, cleaning_base + '.txt')
+    fixture_clean = os.path.join(fixtures_dir, cleaning_base)
     assert is_same_file(cleaning_file, fixture_clean)
 
 
@@ -31,5 +31,5 @@ def test_write_test_cleaning_script(build_class, fixtures_dir):
     test_cleaning_base = os.path.splitext(
         os.path.basename(test_cleaning_file))[0]
     fixture_test_clean = os.path.join(
-        fixtures_dir, test_cleaning_base + '.txt')
+        fixtures_dir, test_cleaning_base)
     assert is_same_file(test_cleaning_file, fixture_test_clean)
